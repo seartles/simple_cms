0b7dfe49d289da4a74d294624d3c23c7d8b5dff7
Nevin Apondo <nevin@nevins-mac-pro.home>
2014-08-18 23:43:10 -0400

    Section_controller:actions and views added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'demo#index'
+  root 'subjects#index'
   match ':controller(/:action(/:id))', :via=>[:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
